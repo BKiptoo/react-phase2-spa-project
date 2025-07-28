ff2e81e8058125881a4208442ae647aac56d405c
BKiptoo <bettsonk@gmail.com>
2025-07-28 14:31:10 +0300

    header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,6 @@ function Header() {
     <header className="header">
       <div className="header-container">
         <h1 className="header-title">Task Manager Pro</h1>
-        <nav className="header-nav">
-          <ul className="nav-list">
-            <li><a href="#tasks" className="nav-link">Tasks</a></li>
-            <li><a href="#users" className="nav-link">Users</a></li>
-            <li><a href="#about" className="nav-link">About</a></li>
-          </ul>
-        </nav>
       </div>
     </header>
   );
